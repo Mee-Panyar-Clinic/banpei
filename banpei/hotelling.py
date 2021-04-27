47df5e1108957735b672b03e7ba629dfd7c93fff
Hunter Whaples <hwhaples@g.hmc.edu>
2021-04-27 17:19:22 -0400

    Updated Hotelling, Added Demo

--- banpei/hotelling.py
+++ banpei/hotelling.py
@@ -7,7 +7,7 @@ class Hotelling(BaseModel):
     def __init__(self):
         pass
 
-    def detect(self, data, threshold):
+    def detect(self, data, threshold, trained_mean=None,trained_var=None):
         """
         Parameters
         ----------
@@ -15,6 +15,10 @@ class Hotelling(BaseModel):
                Input array or object that can be converted to an array.
         threshold : float
 
+        trained_mean (optional): float
+                Input an expected mean, otherwise mean is derived from data
+        trained_var (optional): float
+                Input an expected std, otherwise variance is derived from data
         Returns
         -------
         List of tuples where each tuple contains index number and anomalous value.
@@ -25,13 +29,25 @@ class Hotelling(BaseModel):
         abn_th = stats.chi2.interval(1-threshold, 1)[1]
 
         # Covert raw data into the degree of abnormality
-        avg = np.average(data)
-        var = np.var(data)
+        if trained_mean is None:
+            avg = np.average(data)
+        else:
+            avg = trained_mean
+        
+        if trained_var is None:
+            var = np.var(data)
+        else:
+            var = trained_var
         data_abn = [(x - avg)**2 / var for x in data]
 
-        # Abnormality determination
+        # Return abnormality score and threshold detection
         result = []
-        for (index, x) in enumerate(data_abn):
+        for i in range(len(data_abn)):
+            x = data_abn[i]
+            flag = 0
             if x > abn_th:
-                result.append((index, data[index]))
-        return result
+                flag = 1
+            entry = [x, flag]
+            result.append(entry)
+        
+        return np.array(result)
